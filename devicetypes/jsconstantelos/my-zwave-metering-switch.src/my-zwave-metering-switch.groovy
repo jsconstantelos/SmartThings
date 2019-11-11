1579d1894d849050cacb46f2b91785f245450ea9
John Constantelos <jsconst@gmail.com>
2019-11-11 17:47:15 -0500

    tweaks

--- devicetypes/jsconstantelos/my-zwave-metering-switch.src/my-zwave-metering-switch.groovy
+++ devicetypes/jsconstantelos/my-zwave-metering-switch.src/my-zwave-metering-switch.groovy
@@ -249,7 +249,7 @@ def configure() {
         result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 91, size: 1, scaledConfigurationValue: 1)))	// If parameter 90 is 1, don't send unless watts have changed by x amount
         result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 92, size: 1, scaledConfigurationValue: 1)))	// If parameter 90 is 1, don't send unless watts have changed by x percent
 		result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 101, size: 4, scaledConfigurationValue: 12)))	// report power in watts
-		result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: 60)))	 // every 1 min
+		result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 111, size: 4, scaledConfigurationValue: 5)))	 // every 1 min (default), set to 5 seconds.
 	} else if (zwaveInfo.mfr == "010F" && zwaveInfo.prod == "1801" && zwaveInfo.model == "1000") { // Fibaro Wall Plug UK
 		result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 11, size: 1, scaledConfigurationValue: 2))) // 2% power change results in report
 		result << response(encap(zwave.configurationV1.configurationSet(parameterNumber: 13, size: 2, scaledConfigurationValue: 5*60))) // report every 5 minutes
