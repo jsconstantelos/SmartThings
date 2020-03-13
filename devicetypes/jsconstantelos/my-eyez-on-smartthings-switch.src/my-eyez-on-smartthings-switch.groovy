cdf0a65a16dd32e6476a442be959daeebb3179ee
John C <jsconst@gmail.com>
2020-03-13 16:02:55 -0400

    Tweaks

--- devicetypes/jsconstantelos/my-eyez-on-smartthings-switch.src/my-eyez-on-smartthings-switch.groovy
+++ devicetypes/jsconstantelos/my-eyez-on-smartthings-switch.src/my-eyez-on-smartthings-switch.groovy
@@ -1,4 +1,4 @@
-/**
+/*
  * Author: Vadim Bachmutsky
  * Program: Eyez-On SmartThings Switch
  * Version: 1.0
@@ -8,21 +8,18 @@
  * the use of the Eyez-On server to enable automation for arming/disarming system.
  *
  * TODO: Update switch state based on system status (feedback loop).
- */
-
-/**
- *	Copyright 2020 Vadim Bachmutsky
  *
- *	Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *	in compliance with the License. You may obtain a copy of the License at:
+ * Copyright 2020 Vadim Bachmutsky
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at:
  *
  *		http://www.apache.org/licenses/LICENSE-2.0
  *
- *	Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *	on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *	for the specific language governing permissions and limitations under the License.
- */
-
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ * for the specific language governing permissions and limitations under the License.
+*/
 
 /* 
 APP CONSTANTS (how ST doesn't support one of the most basic constructs of
@@ -75,6 +72,7 @@ preferences {
         input "part", "number", title: "Partition #", required: true
         input "pin", "number", title: "Disarm PIN", required: true
         input "mode", "enum", title: "Arm Mode", options: [ARM_MODE_STAY(), ARM_MODE_AWAY()]
+        input "exitDelay", "number", title: "System exit delay (in seconds)", range: "1..120", required: true
     }
 }
 
@@ -86,12 +84,7 @@ metadata {
         capability "Refresh"
         capability "Polling"
         capability "Health Check"
-        
         command "getSystemStatus"
-    }
-
-    // simulator metadata
-    simulator {
     }
 
     // UI tile definitions
@@ -100,8 +93,8 @@ metadata {
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
 				attributeState "on", label:'Armed', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00A0DC"
 				attributeState "off", label:'Disarmed', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff"
-				attributeState "busy", label:'Busy', icon:"st.switches.light.on", backgroundColor:"#00A0DC"
-				attributeState "exiting", label:'Exiting', icon:"st.switches.light.on", backgroundColor:"#ffffff"
+				attributeState "busy", label:'Busy', icon:"st.switches.light.on", backgroundColor:"#ffa81e"
+				attributeState "exiting", label:'Exiting', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#ffa81e"
 			}
 		}
 		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
@@ -116,7 +109,6 @@ def initialize() {
 	sendEvent(name: "DeviceWatch-Enroll", value: "{\"protocol\": \"LAN\", \"scheme\":\"untracked\", \"hubHardwareId\": \"${device.hub.hardwareID}\"}", displayed: false)
     sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
     sendEvent(name: "healthStatus", value: "online")
-
 }
 
 void installed() {
@@ -172,26 +164,25 @@ def performOperation(operation) {
     log.info "Received request to perform operation: ${operation}"
     try {
      	def random = new Random().nextInt(99999999) + 1
-
+        def waitTime = exitDelay
         def path = "${EYEZON_URI()}${EYEZON_PATH()}?mid=${settings.mid}&action=s&did=${settings.did}&type=15&dmdben=f&rand=${random}"
         def body = "part=${settings.part}&pin=${settings.pin}"
-        // def body = "extaction=${operation}&part=${settings.part}"
-        // def body = "extaction=${operation}&part=${settings.part}&pin=${settings.pin}"
-        // def body = "extaction=dohpincommand&hexaction=${operation}&part=${settings.part}&pin=${settings.pin}"
-        
         if (operation == OPERATION_DISARM()) {
         	body += "&extaction=hdisarm"
+            sendEvent(name: "switch", value: "busy")
+            waitTime = 5  // time in seconds
         } else if (operation == OPERATION_ARM_STAY()) {
         	body += "&extaction=dohpincommand&hextaction=harmstay"
+            sendEvent(name: "switch", value: "exiting")
         } else {
         	body += "&extaction=dohpincommand&hextaction=harmaway"
+            sendEvent(name: "switch", value: "exiting")
         }
-
-        log.info "Path: ${path}"
-        log.info "Body: ${body}"
-
+        //log.info "Path: ${path}"
+        //log.info "Body: ${body}"
         httpPost(path, body)
-        log.info "Operation ${operation} performed successfully"
+        log.info "Operation ${operation} successfully submitted, now waiting for system exit/disarm delay (${waitTime} seconds for exit delay, 5 seconds for disarm delay) for confirmation..."
+        runIn(waitTime, getSystemStatus, [overwrite: true])  // wait for system delay and then get system status to update the tile, and overwrite any pending schedules
     } catch (e) {
         log.error "Unable to perform operation ${operation}: POST failed.", e
     }
@@ -199,27 +190,22 @@ def performOperation(operation) {
 
 def on() {
     log.info "Received request to arm system. Mode: ${settings.mode}"
-    
     def systemStatus = getSystemStatus()
     if (systemStatus != STATUS_READY()) {
     	log.error "Cannot arm system. Expecting status ${STATUS_READY()}. Actual status: ${systemStatus}"
         return
     }
-    
     def operation = settings.mode == 'Away' ? OPERATION_ARM_AWAY() : OPERATION_ARM_STAY()
     performOperation(operation)
-//    getSystemStatus()
 }
 
 def off() {
     log.info "Received request to disarm system. Mode: ${settings.mode}"
-
     def systemStatus = getSystemStatus()
     if (![STATUS_AWAY_ARMED(), STATUS_STAY_ARMED(), STATUS_EXIT_DELAY()].contains(systemStatus)) {
     	log.error "Cannot disarm system: System is in invalid state: ${systemStatus}"
         return
     }
-    
     if (systemStatus == STATUS_STAY_ARMED() && settings.mode == ARM_MODE_AWAY()) {
     	log.error "Cannot disarm system: Expecting status ${STATUS_AWAY_ARMED()}. Actual status: ${systemStatus}"
         return
@@ -227,9 +213,7 @@ def off() {
     	log.error "Cannot disarm system: Expecting status ${STATUS_STAY_ARMED()}. Actual status: ${systemStatus}"
         return
     }
-    
     performOperation(OPERATION_DISARM())
-//    getSystemStatus()
 }
 
 def refresh() {
