24fd5f993f7dc385809d8448c8b2299dc0a0f4c7
John C <jsconst@gmail.com>
2020-03-30 20:45:38 -0400

    Tweaks

--- devicetypes/jsconstantelos/my-xiaomi-mijia-smart-light-sensor.src/my-xiaomi-mijia-smart-light-sensor.groovy
+++ devicetypes/jsconstantelos/my-xiaomi-mijia-smart-light-sensor.src/my-xiaomi-mijia-smart-light-sensor.groovy
@@ -11,7 +11,6 @@
  *  for the specific language governing permissions and limitations under the License.
  */
 
-import groovy.json.JsonOutput
 import physicalgraph.zigbee.zcl.DataType
 
 metadata {
