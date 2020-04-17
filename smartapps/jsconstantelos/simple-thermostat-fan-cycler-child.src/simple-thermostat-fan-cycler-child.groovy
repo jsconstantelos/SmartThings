32e1a0c66c3354937242f8866e2f105c03ce89f7
John C <jsconst@gmail.com>
2020-04-17 12:56:06 -0400

    Tweaka

--- smartapps/jsconstantelos/simple-thermostat-fan-cycler-child.src/simple-thermostat-fan-cycler-child.groovy
+++ smartapps/jsconstantelos/simple-thermostat-fan-cycler-child.src/simple-thermostat-fan-cycler-child.groovy
@@ -1,23 +1,3 @@
-/**
- *  Simple Thermostat Fan Cycler Child
- *
- *  Copyright 2019 J.Constantelos
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- *  Revision History
- *  ----------------
- *  04-09-2019 : Initial release
- *
- */
-
 definition(
     name: "Simple Thermostat Fan Cycler Child",
     namespace: "jsconstantelos",
@@ -36,10 +16,6 @@ preferences {
         input "onMinutes", "number", title: "Stay on for how many minutes?"
         input "offMinutes", "number", title: "Stay off for how many minutes?"
     }
-    section("Only for this mode") {
-        input "activeMode", "enum", title: "Which mode?", multiple:false, 
-        metadata:[values:["Home","Away","Night"]]
-    }
 }
 
 def installed() {
@@ -56,32 +32,17 @@ def updated() {
 
 def initialize() {
 	log.debug "Initializing..."
-    subscribe(location, changedLocationMode)
     onCycleTimer()
 }
 
-def changedLocationMode(evt) {
-	def curMode = location.currentMode
-	log.debug "Checking current mode: ${curMode}"
-    unschedule()
-	onCycleTimer()
-}
-
 def onCycleTimer() {
-	log.debug "Setting ON timer..."
-    def curMode = location.currentMode
-    log.debug "Checking current mode: ${curMode}"
-	if (curMode == activeMode) {
-    	log.debug "We're scheduling now because the modes matched..."
-		runIn(onMinutes * 60, offCycleTimer)
-        thermostats?.fanOn()
-    } else {
-        log.debug "We're not scheduling anything because the modes didn't match..."
-	}
+	log.debug "Setting fan(s) to ON and scheduing OFF/AUTO timer..."
+	runIn(onMinutes * 60, offCycleTimer)
+	thermostats?.fanOn()
 }
 
 def offCycleTimer() {
-	log.debug "Setting OFF timer..."
+	log.debug "Setting fan(s) to AUTO and scheduing ON timer..."
     runIn(offMinutes * 60, onCycleTimer)
     thermostats?.fanAuto()
 }
