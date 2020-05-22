91674dfe4919f84dd28cc8aa072317d4e46ca2a2
John C <jsconst@gmail.com>
2020-05-22 06:56:08 -0400

    See code change log

--- devicetypes/jsconstantelos/my-xiaomi-mijia-smart-light-sensor.src/my-xiaomi-mijia-smart-light-sensor.groovy
+++ devicetypes/jsconstantelos/my-xiaomi-mijia-smart-light-sensor.src/my-xiaomi-mijia-smart-light-sensor.groovy
@@ -31,8 +31,8 @@ metadata {
     }
 
 	preferences {
-    	input "minReportSeconds", "number", title: "Min Report Time (0 to 3600 sec)", description: "Minimum seconds? (10 is default)", defaultValue: 10, range: "0..3600", required: true, displayDuringSetup: true
-        input "rawChange", "number", title: "Amount of change in raw data (1 to 1000)", description: "Amount of change? (21 is default)", defaultValue: 21, range: "1..1000", required: true, displayDuringSetup: true
+    	input "minReportSeconds", "number", title: "Min Report Time (0 to 3600 sec)", description: "Minimum seconds? (10 is default)", defaultValue: 10, range: "0..3600", required: false, displayDuringSetup: true
+        input "rawChange", "number", title: "Amount of change in raw data (1 to 1000)", description: "Amount of change? (21 is default)", defaultValue: 21, range: "1..1000", required: false, displayDuringSetup: true
 	}
 
 	fingerprint profileId: "0104", inClusters: "0000,0400,0003,0001", outClusters: "0003", manufacturer: "LUMI", model: "lumi.sen_ill.mgl01", deviceJoinName: "Xiaomi Mijia Smart Home Light Sensor", ocfDeviceType: "oic.r.sensor.illuminance"    
